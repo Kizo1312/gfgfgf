1cc1f9164fe1f81b5945b5d7abf83a290ab5d168
Toma Ilcic <toma.ilcic@gmail.com>
2023-08-31 20:25:18 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <hi>Tomina stranica</hi>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <main></main>
     </div>
   );
 }
